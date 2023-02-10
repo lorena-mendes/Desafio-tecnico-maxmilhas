765ec977b58bbbf8322d503b2b2e7f453678d76a
lorena-mendes <lorena.lccma@gmail.com>
2023-02-10 14:18:24 -0300

    fix: solve lint errors

--- src/db/connection.js
+++ src/db/connection.js
@@ -8,7 +8,7 @@ const connection = mysql.createPool({
   database: 'list-resttrict',
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0,
+  queueLimit: 0
 });
 
 module.exports = connection;
